193e868165c9dd5d17a25501aa065b5a263ee3b8
Anand Seshadri <78548214+anandsesha@users.noreply.github.com>
2024-02-19 23:54:35 -0500

    initial

---
+++ block-BRaaao/article-card/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from '../src/components/App';
+import './styles/index.css';
+
+ReactDOM.render(<h1> Hello React</h1>, document.getElementById('root'));
