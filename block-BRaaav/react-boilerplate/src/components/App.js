944c572d7a0208e2cf9c991c7ecfec86e7651afb
Anand Seshadri <78548214+anandsesha@users.noreply.github.com>
2024-03-02 22:23:54 -0500

    Hydro React Page!

---
+++ block-BRaaav/react-boilerplate/src/components/App.js
@@ -0,0 +1,23 @@
+import Header from '../components/Header';
+import Hero from './Hero';
+import About from './About';
+import Blog from './Blog';
+import Work from './Work';
+import Contact from './Contact';
+import Footer from './Footer';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Hero />
+      <About />
+      <Blog />
+      <Work />
+      <Contact />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
